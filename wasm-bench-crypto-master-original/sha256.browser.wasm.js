63a075b4af331dddc8c580aaddc94fabf2322677
hopefuel <hopefuel@outlook.com>
2020-04-17 04:05:41 -0400

    Auto update 5/11, and bug fix in 4/11

--- wasm-bench-crypto-master-original/sha256.browser.wasm.js
+++ wasm-bench-crypto-master-original/sha256.browser.wasm.js
@@ -70,13 +70,13 @@ function onRuntimeInitialized() {
       benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash the 100 byte string 1 time on average is: <span id="t1_ten_div"></span> ms.<br></div>');
       benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash a 100 byte string 10 times is: <span id="t1_ten"></span> ms<br></div>');
 
-      document.getElementById("t1_ten").innerHTML = t1;
-      document.getElementById("t2_ten").innerHTML = t2;
-      document.getElementById("t3_ten").innerHTML = t3;
+      document.getElementById("t1_ten").innerHTML = t1.toFixed(3);
+      document.getElementById("t2_ten").innerHTML = t2.toFixed(3);
+      document.getElementById("t3_ten").innerHTML = t3.toFixed(3);
 
-      document.getElementById("t1_ten_div").innerHTML = t1/10;
-      document.getElementById("t2_ten_div").innerHTML = t2/10;
-      document.getElementById("t3_ten_div").innerHTML = t3/10;
+      document.getElementById("t1_ten_div").innerHTML = (t1/10).toFixed(3);
+      document.getElementById("t2_ten_div").innerHTML = (t2/10).toFixed(3);
+      document.getElementById("t3_ten_div").innerHTML = (t3/10).toFixed(3);
 
     }else if (times == 100){
 
@@ -86,22 +86,22 @@ function onRuntimeInitialized() {
 
       var benchmarks = document.getElementById('bench100');
       benchmarks.insertAdjacentHTML('afterend','<br>');
-      benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash the 1 mb byte string 1 time on average is: <span id="t3_hun_div"></span> ms.<br></div>');
-      benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash a 1 mb byte string 100 times is: <span id="t3_hun"></span> ms<br></div>');
+      benchmarks.insertAdjacentHTML('afterend','<div id="results">The time it took to hash the 1 mb byte string 1 time on average is: <span id="t3_hun_div"></span> ms.<br></div>');
+      benchmarks.insertAdjacentHTML('afterend','<div id="results">The time it took to hash a 1 mb byte string 100 times is: <span id="t3_hun"></span> ms<br></div>');
       benchmarks.insertAdjacentHTML('afterend','<br>');
-      benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash the 1 kb string 1 time on average is: <span id="t2_hun_div"></span> ms.<br></div>');
-      benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash a 1 kb string 100 times is: <span id="t2_hun"></span> ms<br></div>');
+      benchmarks.insertAdjacentHTML('afterend','<div id="results">The time it took to hash the 1 kb string 1 time on average is: <span id="t2_hun_div"></span> ms.<br></div>');
+      benchmarks.insertAdjacentHTML('afterend','<div id="results">The time it took to hash a 1 kb string 100 times is: <span id="t2_hun"></span> ms<br></div>');
       benchmarks.insertAdjacentHTML('afterend','<br>');
-      benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash the 100 byte string 1 time on average is: <span id="t1_hun_div"></span> ms.<br></div>');
-      benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash a 100 byte string 100 times is: <span id="t1_hun"></span> ms<br></div>');
+      benchmarks.insertAdjacentHTML('afterend','<div id="results">The time it took to hash the 100 byte string 1 time on average is: <span id="t1_hun_div"></span> ms.<br></div>');
+      benchmarks.insertAdjacentHTML('afterend','<div id="results">The time it took to hash a 100 byte string 100 times is: <span id="t1_hun"></span> ms<br></div>');
 
-      document.getElementById("t1_hun").innerHTML = t1;
-      document.getElementById("t2_hun").innerHTML = t2;
-      document.getElementById("t3_hun").innerHTML = t3;
+      document.getElementById("t1_hun").innerHTML = t1.toFixed(3);
+      document.getElementById("t2_hun").innerHTML = t2.toFixed(3);
+      document.getElementById("t3_hun").innerHTML = t3.toFixed(3);
 
-      document.getElementById("t1_hun_div").innerHTML = t1/100;
-      document.getElementById("t2_hun_div").innerHTML = t2/100;
-      document.getElementById("t3_hun_div").innerHTML = t3/100;
+      document.getElementById("t1_hun_div").innerHTML = (t1/100).toFixed(3);
+      document.getElementById("t2_hun_div").innerHTML = (t2/100).toFixed(3);
+      document.getElementById("t3_hun_div").innerHTML = (t3/100).toFixed(3);
 
 
     }else if (times == 1000){
@@ -121,13 +121,13 @@ function onRuntimeInitialized() {
       benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash the 100 byte string 1 time on average is: <span id="t1_thou_div"></span> ms.<br></div>');
       benchmarks.insertAdjacentHTML('afterend','<div>The time it took to hash a 100 byte string 1000 times is: <span id="t1_thou"></span> ms<br></div>');
 
-      document.getElementById("t1_thou").innerHTML = t1;
-      document.getElementById("t2_thou").innerHTML = t2;
-      document.getElementById("t3_thou").innerHTML = t3;
+      document.getElementById("t1_thou").innerHTML = t1.toFixed(3);
+      document.getElementById("t2_thou").innerHTML = t2.toFixed(3);
+      document.getElementById("t3_thou").innerHTML = t3.toFixed(3);
 
-      document.getElementById("t1_thou_div").innerHTML = t1/1000;
-      document.getElementById("t2_thou_div").innerHTML = t2/1000;
-      document.getElementById("t3_thou_div").innerHTML = t3/1000;
+      document.getElementById("t1_thou_div").innerHTML = (t1/1000).toFixed(3);
+      document.getElementById("t2_thou_div").innerHTML = (t2/1000).toFixed(3);
+      document.getElementById("t3_thou_div").innerHTML = (t3/1000).toFixed(3);
     }
   }
 }
